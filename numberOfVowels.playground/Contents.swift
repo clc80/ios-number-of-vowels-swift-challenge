55b3eb3a2aaeee04d5c5e0674beed22384424378
Claudia Maciel <clc_80@yahoo.com>
2020-01-27 19:24:18 -0800

    Code challenge completed

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,21 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    //to keep track of the vowel count
+    var count:Int = 0
     
+    for letter in string {
+        //convert to lowercase to have less comparisons
+        let newLetter = letter.lowercased()
+        //check to see if they want to include y if so does it match the letter y?
+        if isYAVowel && newLetter == "y" {
+            count += 1
+        //does the letter match to a vowel
+        } else if newLetter == "a" || newLetter == "e" || newLetter == "i" || newLetter == "o" || newLetter == "u" {
+            count += 1
+        }
+    }
+    return count
 }
+
+print(numberOfVowels(in: "Polly wants A cracker!", isYAVowel: true))
